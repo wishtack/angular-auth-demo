129b3fd33b2cf719964a7e1b9e0f520df7f8a9ba
Younes JAAIDI <yjaaidi@gmail.com>
2017-07-10 12:40:12 +0200

    AuthHttp dynamic method wrapping.

--- src/app/auth/auth-http.ts
+++ src/app/auth/auth-http.ts
@@ -6,7 +6,7 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Http, RequestOptionsArgs, Response } from '@angular/http';
+import { Headers, Http, RequestOptionsArgs, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import { Session } from './session';
@@ -14,32 +14,74 @@ import { Session } from './session';
 @Injectable()
 export class AuthHttp {
 
+    delete: (url: string, options?: RequestOptionsArgs) => Observable<Response>;
+    get: (url: string, options?: RequestOptionsArgs) => Observable<Response>;
+    head: (url: string, options?: RequestOptionsArgs) => Observable<Response>;
+    options: (url: string, options?: RequestOptionsArgs) => Observable<Response>;
+
+    patch: (url: string, body: any, options?: RequestOptionsArgs) => Observable<Response>;
+    post: (url: string, body: any, options?: RequestOptionsArgs) => Observable<Response>;
+    put: (url: string, body: any, options?: RequestOptionsArgs) => Observable<Response>;
+
+    private _headerName = 'Authorization';
+    private _tokenKey = 'Bearer';
+
     constructor(private _http: Http, private _session: Session) {
+
+        /* Methods without body. */
+        for (let method of ['delete', 'get', 'head', 'options']) {
+            this._decorateMethod({
+                method: method,
+                hasBody: false
+            });
+        }
+
+        /* Methods with body. */
+        for (let method of ['patch', 'post', 'put']) {
+            this._decorateMethod({
+                method: method,
+                hasBody: true
+            });
+        }
+
     }
 
-    get(url: string, options?: RequestOptionsArgs): Observable<Response> {
-        return this._overrideOptions(options)
-            .switchMap((_options) => this._http.get(url, _options));
+    private _decorateMethod({method, hasBody}) {
+
+        let optionsPosition = hasBody ? 2 : 1;
+
+        this[method] = (...args) => this._overrideOptions({args, optionsPosition})
+            .switchMap((_args) => this._http[method](..._args));
+
+
     }
 
-    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Response> {
-        return this._overrideOptions(options)
-            .switchMap((_options) => this._http.post(url, body, _options));
-    }
-
-    private _overrideOptions(options): Observable<RequestOptionsArgs> {
+    private _overrideOptions({args, optionsPosition}) {
 
         return this._session.getToken()
             .map((token) => {
+
+                let _args = [...args];
+                let options = _args[optionsPosition];
+
                 if (token != null) {
+
+                    /* Add authorization header to options .*/
                     options = {...options};
-                    options.headers = {...options.headers, ...{
-                        'Authorization': `Bearer ${token}`
-                    }};
+                    options.headers = new Headers(options.headers);
+                    options.headers.append(
+                        this._headerName,
+                        `${this._tokenKey} ${encodeURIComponent(token)}`
+                    );
+
+                    /* Replace options parameter. */
+                    _args[optionsPosition] = options;
                 }
-                return options;
+
+                return _args;
+
             });
 
     }
-    
+
 }
